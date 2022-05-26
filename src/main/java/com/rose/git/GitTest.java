edcfb77f24b2b3b0bdabcc9803e1359859b7f95e
zjh <1352116790@qq.com>
2022-05-26 22:10:26 +0800

    first commit

---
+++ src/main/java/com/rose/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.rose.git;
+
+/**
+ * @author zjh
+ * @date 2022/5/26 21:55
+ * @description
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello pcy!");
+    }
+
+}
